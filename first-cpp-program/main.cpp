80fa5cc094b923ae9a54e2d7061a94c2e9ed8a62
Julio Jimenez <julio@julioj.com>
2022-04-10 18:16:41 -0400

    first-cpp-program
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ first-cpp-program/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    std::cout << "Julio" << std::endl;
+    return 0;
+}
