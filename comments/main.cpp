30d54c4d43c762c83606a129c8ecdecb2f97cb65
Julio Jimenez <julio@julioj.com>
2022-04-11 21:08:44 -0400

    comments
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ comments/main.cpp
@@ -0,0 +1,15 @@
+// This brings in the iostream library
+#include <iostream>
+
+/*
+    This is a block comment
+    and can have
+    multiple lines
+*/
+
+int main() {
+    // Print hello world
+    std::cout << "hello world" << std::endl;
+    return 0;
+    // end
+}
